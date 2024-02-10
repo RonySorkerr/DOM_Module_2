e7d68a16c2fb52882bc9984462de0c12c2dfa85f
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-10 19:46:39 +0600

    addeventListener

--- event.js
+++ event.js
@@ -1,8 +1,26 @@
 console.log('this is alogn with js file');
 
-// option 4 to add events
+// option 3 another
 
 const purpleButton = document.getElementById('pink');
 purpleButton.onclick = function purpleClicked (){
     document.body.style.backgroundColor= 'pink';
 }
+
+// option 4
+
+
+const pinkButton = document.getElementById('tomato');
+pinkButton.addEventListener('click', makeTomato)
+
+function makeTomato (){
+    document.body.style.backgroundColor= 'tomato';
+}
+
+// option 5 
+
+const makeGreen = document.getElementById('green');
+makeGreen.addEventListener('click', 
+function greenButton(){
+    document.body.style.backgroundColor= 'green';
+})
