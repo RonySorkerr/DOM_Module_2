d94646003db6d688e69951f9cd62274a93cbda2b
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-10 19:27:17 +0600

    class two done

--- event.js
+++ event.js
@@ -1,1 +1,8 @@
 console.log('this is alogn with js file');
+
+// option 4 to add events
+
+const purpleButton = document.getElementById('pink');
+purpleButton.onclick = function purpleClicked (){
+    document.body.style.backgroundColor= 'pink';
+}
